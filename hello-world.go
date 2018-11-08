5eeae324d354de649b0433f6da67940a4ea0cbd4
Giacomo Ghidini <giacomo@ankr.network>
2018-11-08 10:03:31 -0800

    [SWDEV-73] Break the build

--- hello-world.go
+++ hello-world.go
@@ -6,7 +6,7 @@ import (
     "net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+fun handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
